f3b3634480a0712ac9f5b10b9856a35578b61a0c
HongduanTian <hongduan.tian@gmail.com>
2024-02-19 16:51:18 +0800

    second commit

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 ## this file is based on version 13.7 of python telegram chatbot
 ## and version 1.26.18 of urllib3
 ## chatbot.py
+import os
 import telegram
 from telegram.ext import Updater, MessageHandler, Filters
 # The messageHandler is used for all message updates
-import configparser
+#import configparser
 import logging
 
 #import subprocess
@@ -17,10 +18,11 @@ import requests
 
 def main():
     # Load your token and create an Updater for your Bot
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    print(config['TELEGRAM']['ACCESS_TOKEN'])
-    updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+    #config = configparser.ConfigParser()
+    #config.read('config.ini')
+    #print(config['TELEGRAM']['ACCESS_TOKEN'])
+    #updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+    updater = Updater(token=(os.environ['ACCESS_TOKEN']), use_context=True)
     dispatcher = updater.dispatcher
     # You can set this logging module,
     # so you will know when and why things do not work as expected
@@ -30,7 +32,8 @@ def main():
     # echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
     # dispatcher.add_handler(echo_handler)
     global chatgpt
-    chatgpt = HKBU_GPT(config)
+    #chatgpt = HKBU_GPT(config)
+    chatgpt = HKBU_GPT()
     chatgpt_handler = MessageHandler(Filters.text & (~Filters.command),equiped_chatgpt)
     dispatcher.add_handler(chatgpt_handler)
     # To start the bot:
@@ -51,17 +54,21 @@ def equiped_chatgpt(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
 
 class HKBU_GPT():
+    #def __init__(self, config='./config.ini'):
     def __init__(self, config='./config.ini'):
-        if type(config) == str:
-            self.config = configparser.ConfigParser()
-            self.config.read(config)
-        elif type(config) == configparser.ConfigParser:
-            self.config = config
+        #if type(config) == str:
+        #    self.config = configparser.ConfigParser()
+        #    self.config.read(config)
+        #elif type(config) == configparser.ConfigParser:
+        #    self.config = config
+        pass
     
     def submit(self, message):
         conversation = [{"role": "user", "content": message}]
-        url = (self.config['CHATGPT']['BASICURL']) + "/deployments/" + (self.config['CHATGPT']['MODELNAME']) + "/chat/completions/?api-version=" + (self.config['CHATGPT']['APIVERSION'])
-        headers = { 'Content-Type': 'application/json', 'api-key': (self.config['CHATGPT']['ACCESS_TOKEN']) }
+        #url = (self.config['CHATGPT']['BASICURL']) + "/deployments/" + (self.config['CHATGPT']['MODELNAME']) + "/chat/completions/?api-version=" + (self.config['CHATGPT']['APIVERSION'])
+        url = (os.environ['BASICURL']) + "/deployments/" + (os.environ['MODELNAME']) + "/chat/completions/?api-version=" + (os.environ['APIVERSION'])
+        #headers = { 'Content-Type': 'application/json', 'api-key': (self.config['CHATGPT']['ACCESS_TOKEN']) }
+        headers = { 'Content-Type': 'application/json', 'api-key': (os.environ['ACCESS_TOKEN']) }
         payload = { 'messages': conversation }
         response = requests.post(url, json=payload, headers=headers)
         if response.status_code == 200:
